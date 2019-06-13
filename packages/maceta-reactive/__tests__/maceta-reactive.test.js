fd4952a6c6f0068c853c00bdaa27172d570ba2f0
John Henson <contact@mabujo.com>
2019-06-13 22:10:02 +0200

    Manage dependencies, add testcheck for generative testing. Fixes #1

--- packages/maceta-reactive/__tests__/maceta-reactive.test.js
+++ packages/maceta-reactive/__tests__/maceta-reactive.test.js
@@ -1,6 +1,9 @@
-'use strict';
+import { install } from 'jasmine-check';
 
 import { isObject } from '../src/maceta-reactive';
+
+// install testcheck utils to global scope
+install();
 
 describe('when checking if a variable is an object', () => {
   it('should return true when passed an object', () => {
@@ -8,5 +11,11 @@ describe('when checking if a variable is an object', () => {
     const testObjConstructor = new Object();
     expect(isObject(testObj)).toBeTruthy();
     expect(isObject(testObjConstructor)).toBeTruthy();
+  });
+  check.it('returns false when passed an array', gen.array, arrays => {
+    expect(isObject(arrays)).toBeFalsy();
+  });
+  check.it('returns false when passed a number', gen.number, numbers => {
+    expect(isObject(numbers)).toBeFalsy();
   });
 });
